981aeb915b9da0fbcde96e96f9f5e4a4ef9cf081
taavirehemagi <taavirehemagi@gmail.com>
2015-06-01 15:19:54 +0300

    Make tag notification bubble scale correctly.

--- Joey/UI/Views/NotificationImageView.cs
+++ Joey/UI/Views/NotificationImageView.cs
@@ -4,6 +4,7 @@ using Android.Graphics.Drawables;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using System;
 
 namespace Toggl.Joey.UI.Views
 {
@@ -49,15 +50,13 @@ namespace Toggl.Joey.UI.Views
             var iconBitmap = ((BitmapDrawable)currentDrawable).Bitmap;
             iconBitmap = iconBitmap.Copy (Bitmap.Config.Argb8888, true);
 
-            //Make room for bubble.
-            var biggerBitmap = Bitmap.CreateBitmap (iconBitmap.Width + 5, iconBitmap.Height + 10, Bitmap.Config.Argb8888);
-            var canvas = new Canvas (biggerBitmap);
-            canvas.DrawBitmap (iconBitmap, 0, 10, null);
+
             var dm = ctx.Resources.DisplayMetrics;
 
             TextView bubble = new TextView (ctx);
             bubble.Text = bubbleCount.ToString();
-            bubble.TextSize= (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 4, dm);;
+
+            bubble.SetTextSize (ComplexUnitType.Sp, 7.5f);
             bubble.Gravity = GravityFlags.Center;
             bubble.SetTextColor (ctx.Resources.GetColor (Resource.Color.dark_gray_text));
             var bubbleBackground = ctx.Resources.GetDrawable (Resource.Drawable.NotificationBubble);
@@ -70,12 +69,19 @@ namespace Toggl.Joey.UI.Views
             );
 
             bubble.DrawingCacheEnabled = true;
-            bubble.Measure (
-                MeasureSpec.MakeMeasureSpec (0, MeasureSpecMode.Unspecified),
-                MeasureSpec.MakeMeasureSpec (0, MeasureSpecMode.Unspecified)
-            );
+            bubble.Measure (0, 0);
             bubble.Layout (0, 0, bubble.MeasuredWidth, bubble.MeasuredHeight);
             bubble.BuildDrawingCache();
+
+            //Make room for bubble.
+            var biggerBitmap = Bitmap.CreateBitmap (
+                                   iconBitmap.Width + bubble.MeasuredWidth/4,
+                                   iconBitmap.Height + bubble.Height/2,
+                                   Bitmap.Config.Argb8888
+                               );
+            var canvas = new Canvas (biggerBitmap);
+            canvas.DrawBitmap (iconBitmap, 0, (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 4, dm), null);
+
             canvas.DrawBitmap (bubble.DrawingCache, biggerBitmap.Width - bubble.Width, 0, null);
             var drawable = new BitmapDrawable (ctx.Resources, biggerBitmap);
             SetImageDrawable (drawable);
