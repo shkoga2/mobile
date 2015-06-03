1334905748cc576b5cdf711b3da0009ab08030ca
taavirehemagi <taavirehemagi@gmail.com>
2015-06-03 16:16:16 +0300

    Bubble notification refactor.

--- Joey/UI/Views/NotificationImageView.cs
+++ Joey/UI/Views/NotificationImageView.cs
@@ -11,8 +11,12 @@ namespace Toggl.Joey.UI.Views
     public class NotificationImageView : ImageView
     {
         private Context ctx;
-        private int bubbleCount = 5;
-        private Drawable originalDrawable;
+        private int bubbleCount = 0;
+        private readonly Drawable originalDrawable;
+        private TextView bubbleText;
+        private Bitmap baseBitmap;
+        private Drawable endDrawable;
+        private Canvas baseCanvas;
 
         public NotificationImageView (Context context) : base (context)
         {
@@ -32,61 +36,63 @@ namespace Toggl.Joey.UI.Views
             originalDrawable = Drawable;
         }
 
-
         public int BubbleCount
         {
             get {
                 return bubbleCount;
             } set {
                 bubbleCount = value;
-                UpdateBubble();
+                UpdateBubble ();
             }
         }
 
         public void UpdateBubble()
         {
-            var currentDrawable = originalDrawable;
-
-            var iconBitmap = ((BitmapDrawable)currentDrawable).Bitmap;
-            iconBitmap = iconBitmap.Copy (Bitmap.Config.Argb8888, true);
-
-
-            var dm = ctx.Resources.DisplayMetrics;
-
-            TextView bubble = new TextView (ctx);
-            bubble.Text = bubbleCount.ToString();
-
-            bubble.SetTextSize (ComplexUnitType.Sp, 7.5f);
-            bubble.Gravity = GravityFlags.Center;
-            bubble.SetTextColor (ctx.Resources.GetColor (Resource.Color.dark_gray_text));
-            var bubbleBackground = ctx.Resources.GetDrawable (Resource.Drawable.NotificationBubble);
-            bubble.SetBackgroundDrawable (bubbleBackground);
-            bubble.SetPadding (
-                (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 3, dm),
-                0,
-                (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 3, dm),
-                0
-            );
-
-            bubble.DrawingCacheEnabled = true;
-            bubble.Measure (0, 0);
-            bubble.Layout (0, 0, bubble.MeasuredWidth, bubble.MeasuredHeight);
-            bubble.BuildDrawingCache();
-
-            //Make room for bubble.
-            var biggerBitmap = Bitmap.CreateBitmap (
-                                   iconBitmap.Width + bubble.MeasuredWidth/4,
-                                   iconBitmap.Height + bubble.Height/2,
-                                   Bitmap.Config.Argb8888
-                               );
-            var canvas = new Canvas (biggerBitmap);
-            canvas.DrawBitmap (iconBitmap, 0, (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 4, dm), null);
-
-            canvas.DrawBitmap (bubble.DrawingCache, biggerBitmap.Width - bubble.Width, 0, null);
-            var drawable = new BitmapDrawable (ctx.Resources, biggerBitmap);
-            SetImageDrawable (drawable);
+            MakeBubbleText ();
+            if (baseCanvas == null
+                    || (bubbleText != null && bubbleText.Text.Length > bubbleCount.ToString ().Length)) {
+                MakeBaseBitmap ();
+            }
+            baseCanvas.DrawBitmap (bubbleText.DrawingCache, baseBitmap.Width - bubbleText.Width, 0, null);
+            endDrawable = new BitmapDrawable (ctx.Resources, baseBitmap);
+            SetImageDrawable (endDrawable);
         }
 
+        private bool renewBase
+        {
+            get {
+                return (baseCanvas == null || (bubbleText != null && bubbleText.Text.Length > bubbleCount.ToString ().Length)) ? true : false;
+            }
+        }
+
+        private void MakeBubbleText ()
+        {
+            var dm = ctx.Resources.DisplayMetrics;
+            bubbleText = new TextView (ctx);
+            bubbleText.SetTextSize (ComplexUnitType.Sp, 7.5f);
+            bubbleText.Gravity = GravityFlags.Center;
+            bubbleText.SetTextColor (ctx.Resources.GetColor (Resource.Color.dark_gray_text));
+            bubbleText.SetBackgroundResource (Resource.Drawable.NotificationBubble);
+            int padding = (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 3, dm);
+            bubbleText.SetPadding (padding, 0, padding, 0);
+            bubbleText.Text = bubbleCount.ToString();
+            bubbleText.DrawingCacheEnabled = true;
+            bubbleText.Measure (0, 0);
+            bubbleText.Layout (0, 0, bubbleText.MeasuredWidth, bubbleText.MeasuredHeight);
+            bubbleText.BuildDrawingCache();
+        }
+
+        private void MakeBaseBitmap ()
+        {
+            var iconBitmap = ((BitmapDrawable)originalDrawable).Bitmap;
+            iconBitmap = iconBitmap.Copy (Bitmap.Config.Argb8888, true);
+            var dm = ctx.Resources.DisplayMetrics;
+            int top = (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 4, dm);
+            int left = (int)TypedValue.ApplyDimension (ComplexUnitType.Dip, 2, dm);
+            baseBitmap = Bitmap.CreateBitmap (iconBitmap.Width + left, iconBitmap.Height + top, Bitmap.Config.Argb8888);
+            baseCanvas = new Canvas (baseBitmap);
+            baseCanvas.DrawBitmap (iconBitmap, 0, top, null);
+        }
     }
 }
 
